34d7435b8c793dd01ba74f86521f047cf59bec72
pabloamicar <pablo.olmedo@externo.amicar.cl>
2025-02-26 16:24:35 -0300

    Install faker to add random names for contestants

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import { OlimpicGame } from "./Classes/OlimpicGame.class";
 import { Contestant } from "./Classes/Contestant.class";
 import { Countries } from "./types/interface";
+import { Sports } from "./types/interface";
 console.log("Starting the olimpic games!⚽️🏀🏈⚾️🥎");
 
-const sinchornizedSwimming = new OlimpicGame("Nado sincronizado");
+import { faker } from "@faker-js/faker";
+const sinchornizedSwimming = new OlimpicGame(Sports.Karate);
+
 const chileanContestant = new Contestant("Pablinho", Countries.CHILE);
-const americanContestant = new Contestant("James Jameson", Countries.USA);
-const japaneseContestant = new Contestant("Sakamoto", Countries.JAPAN);
+const americanContestant = new Contestant(
+  faker.person.fullName(),
+  Countries.USA
+);
+const japaneseContestant = new Contestant(
+  faker.person.fullName(),
+  Countries.JAPAN
+);
+const germanContestant = new Contestant(
+  faker.person.fullName(),
+  Countries.GERMANY
+);
 
 sinchornizedSwimming.addContestant(chileanContestant);
 sinchornizedSwimming.addContestant(americanContestant);
 sinchornizedSwimming.addContestant(japaneseContestant);
+sinchornizedSwimming.addContestant(germanContestant);
 
 sinchornizedSwimming.playGame();
