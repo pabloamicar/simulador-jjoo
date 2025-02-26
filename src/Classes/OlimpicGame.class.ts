e32f7dcc59434dc6673a7ee6d90d95b7dd4590bd
pabloamicar <pablo.olmedo@externo.amicar.cl>
2025-02-26 16:24:15 -0300

    Showplaces now show more places. Simplified logic of showContestants

--- src/Classes/OlimpicGame.class.ts
+++ src/Classes/OlimpicGame.class.ts
@@ -1,8 +1,9 @@
 import { Contestant } from "./Contestant.class";
+import { Sports } from "../types/interface";
 
 export class OlimpicGame {
   private contestants: Contestant[];
-  constructor(private name: string) {
+  constructor(private name: Sports) {
     this.contestants = [];
   }
 
@@ -14,10 +15,7 @@ export class OlimpicGame {
       return;
     }
     console.log(`Starting the game: ${this.name} !!!`);
-    console.log("Lets see the contestans of this game:");
-    this.contestants.forEach((contestant) => {
-      console.log(contestant.showContestat());
-    });
+    this.showContestans();
     this.assignPlaces();
     console.log("OMG, so amazing game 🤯");
     this.showPlaces();
@@ -27,6 +25,12 @@ export class OlimpicGame {
     contestant.setPlace(0);
     this.contestants.push(contestant);
   }
+  showContestans() {
+    const contestants = this.contestants.map((contestant) => {
+      return contestant.showContestat();
+    });
+    console.table(contestants, ["name"]);
+  }
 
   private assignPlaces() {
     this.contestants.sort(() => Math.random() - 0.5);
@@ -34,9 +38,23 @@ export class OlimpicGame {
 
   private showPlaces() {
     console.log("🏆We got the winners!🏆");
-    console.log(`🥇 1st place: ${this.contestants[0].showContestat()}`);
-    console.log(`🥈 2nd place: ${this.contestants[1].showContestat()}`);
-    console.log(`🥉 3rd place: ${this.contestants[2].showContestat()}`);
+    this.contestants.forEach((contestant, index) => {
+      const { name, country } = contestant.showContestat();
+      switch (index) {
+        case 0:
+          console.log(`🥇 1st place: ${name} from  ${country}`);
+          break;
+        case 1:
+          console.log(`🥈 2nd place: ${name} from  ${country}`);
+          break;
+        case 2:
+          console.log(`🥉 3rd place: ${name} from  ${country}`);
+          break;
+        default:
+          console.log(`🎗  ${index + 1}° place: ${name} from  ${country}`);
+          break;
+      }
+    });
     console.log("Thank you for watching the games! 🎉");
   }
 }
